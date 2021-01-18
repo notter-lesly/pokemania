e2d225e25c7984140d9ba631cc27fb49183260ce
Lesly Notter <notter.lesly@gmail.com>
2021-01-18 09:27:32 +0100

    New components

---
+++ src/components/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+  <footer>
+      <h2>Site map</h2>
+      <div class="footerOrg">
+        <ul>
+          <li><a> Home</a></li>
+          <li><a> Pokemons</a></li>
+          <li><a> Random Pokemon</a></li>
+        </ul>
+        <ul>
+          <li>Contact us</li>
+          <li>Adress: noonono</li>
+          <li>Telephone: 00000</li>
+        </ul>
+      </div>
+    </footer>
+</template>
+
+<script>
+export default{
+  name: "Footer"
+}
+
+</script>
+
+<style scoped>
+
+
+footer {
+  background-color: black;
+  color: white;
+  font-size: 1.5vw;
+  font-family: montserrat;
+  line-height: 2.5vw;
+  width: 100vw;
+}
+footer h2 {
+  text-decoration: underline;
+}
+
+footer a,
+li {
+  text-decoration: none;
+  color: rgb(156, 151, 151);
+}
+
+.footerOrg {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+
+<header>
+      <div class="logo">
+         <router-link to="/"> <img class="pokeball" src="../assets/imgs/pokeball.png" /></router-link>
+        <h1>Pokemania</h1>
+      </div>
+      <ul class="menu">
+     <a><router-link to="/">Home</router-link> </a>
+   <a> <router-link to="/random">Random</router-link></a>
+
+      </ul>
+      </header>
+
+      </template>
+      <script>
+
+export default{
+    name: 'Header'
+}
+          </script>
+<style scoped>
+header {
+  width: 100%;
+  position: fixed;
+  margin: auto;
+  background-color: white;
+  height: 3.5vw;
+  display: flex;
+  justify-content: space-between;
+}
+.pokeball {
+  width: 3.5vw;
+  height: 3.5vw;
+  border: none;
+  margin-top: 0;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+}
+.logo {
+  display: flex;
+}
+.logo h1 {
+  font-family: "Gloria Hallelujah", cursive;
+  font-size: 1.3vw;
+}
+
+.menu {
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 2vw;
+  justify-content: space-around;
+}
+.menu a {
+  text-decoration: none;
+  color: black;
+  font-size: 1.5vw;
+  padding: 0.5vw;
+  font-family: montserrat;
+}
+</style>
