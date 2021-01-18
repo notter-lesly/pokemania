961e2052d299e9f13cbeff49f588dff0da1b3f2a
Lesly Notter <notter.lesly@gmail.com>
2021-01-18 18:08:49 +0100

    added new pokemons

--- src/views/Pokemania.vue
+++ src/views/Pokemania.vue
@@ -3,67 +3,38 @@
 <template>
   <div id="myApp">
     <h1 class="subtitle">{{ subtitle }}</h1>
-    <button>Sort</button>
-
-
-    <section class="pokes">
-      <div>
-        <router-link :to="{ name: 'Detail', params: { name: 'pikachu' } }"
-          ><img src="../assets/imgs/pika.png" />
-        </router-link>
-      </div>
-      <div name="gengar" >
-        <router-link :to="{ name: 'Detail', params: { name: 'gengar' } }"
-          ><img src="../assets/imgs/gengar.png" />
-        </router-link>
-      </div>
-      <div>
-        <router-link :to="{ name: 'Detail', params: { name: 'bulbasaur' } }">
-          <img src="../assets/imgs/bulba.png" />
-        </router-link>
-      </div>
-      <div >
-        <router-link :to="{ name: 'Detail', params: { name: 'squirtle' } }">
-          <img src="../assets/imgs/squirtle.png" />
-        </router-link>
-      </div>
-      <div >
-        <router-link :to="{ name: 'Detail', params: { name: 'charmander' } }">
-          <img src="../assets/imgs/char.png" />
-        </router-link>
-      </div>
-      <div >
-        <router-link :to="{ name: 'Detail', params: { name: 'chikorita' } }">
-          <img src="../assets/imgs/chicorita.png" />
-        </router-link>
-      </div>
-      <div>
-        <router-link :to="{ name: 'Detail', params: { name: 'eevee' } }">
-          <img src="../assets/imgs/eeve.png" />
-        </router-link>
-      </div>
-      <div >
-        <router-link :to="{ name: 'Detail', params: { name: 'jigglypuff' } }">
-          <img src="../assets/imgs/jiggly.png" />
-        </router-link>
-      </div>
-      <div>
-        <router-link :to="{ name: 'Detail', params: { name: 'psyduck' } }">
-          <img src="../assets/imgs/psy.png" />
-        </router-link>
-      </div>
-      <div >
-        <router-link :to="{ name: 'Detail', params: { name: 'pidgeot' } }">
-          <img src="../assets/imgs/pigeot.png" />
-        </router-link>
-      </div>
   
+    
+      <button value="name" @click="sortByName()">Sort by Name</button>
+      <button  value="value" @click="sortByNumber()">Sort by Value</button>
+    
+    
+<div class="wrapper">
+    <section class="pokes" v-for="item in items" :key="item.name">
+      <div>
+        <router-link :to="{ name: 'Detail', params: { name: item.name } }">
+          <img :src="item.img" />
+        </router-link>
+      </div>
+
     </section>
-        </div>
- 
+    </div>
+  </div>
 </template>
 
 <script>
+import _sortBy from 'lodash';
+import pikachu from '../assets/imgs/pika.png'
+import gengar from '../assets/imgs/gengar.png'
+import bulbasaur from '../assets/imgs/bulba.png'
+import squirtle from '../assets/imgs/squirtle.png'
+import charmander from '../assets/imgs/char.png'
+import chicorita from '../assets/imgs/chicorita.png'
+import eevee from '../assets/imgs/eeve.png'
+import jigglypuff from '../assets/imgs/jiggly.png'
+import psyduck from '../assets/imgs/psy.png'
+import pidgeot from '../assets/imgs/pigeot.png'
+
 export default {
   name: "myApp",
 
@@ -79,6 +50,20 @@ export default {
       h1: "WHO IS THIS POKEMON?",
       button: "TRY",
       home: "pokemania.html",
+       currentOrder: 'value',
+            items: [
+                {name: "pikachu", number: "1", img: pikachu},
+                {name: "gengar", number: "2", img: gengar},
+                {name: "bulbasaur", number: "3", img: bulbasaur},
+                {name: "squirtle", number: "4", img: squirtle},
+                {name: "charmander", number: "5", img: charmander},
+                {name: "chikorita", number: "6", img: chicorita},
+                {name: "eevee", number: "7", img: eevee},
+                {name: "jigglypuff", number: "8", img: jigglypuff},
+                {name: "psyduck", number: "9", img: psyduck},
+                {name: "pidgeot", number: "10", img: pidgeot},
+              
+            ]
         
     };
   },
@@ -105,14 +90,21 @@ export default {
           this.image = poke1.sprites.front_default;
         });
     },
-      
-        
+    sortByName() { 
+      var sortedItems = _sortBy(this.items, "name");
+      console.log(sortedItems)
+        },
+    sortByNumber() {
+            this.items = _sortBy(this.items, ['number'],["asc"]);
+            console.log(this.items)
+        }, 
   },
 };
 </script>
 
 <style>
-html, body,
+html,
+body,
 div,
 span,
 applet,
@@ -237,11 +229,11 @@ table {
 }
 
 header {
-  width: 100%;
+ 
   position: fixed;
   margin: auto;
   background-color: white;
-  height: 3.5vw;
+  
   display: flex;
   justify-content: space-between;
 }
@@ -249,9 +241,9 @@ header {
   text-align: center;
   padding-top: 5vw;
   margin-bottom: 2vw;
-  font-size: 2.5vw;
+  font-size: 3vw;
   color: rgba(255, 255, 255, 0.801);
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-family: Montserrat;
 }
 
 #myApp {
@@ -272,11 +264,101 @@ img {
   margin: 1vw;
   border: 2px solid black;
 }
-
-section {
+.wrapper {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   width: 100vw;
+  flex-direction: row;
 }
+.select{
+  background-color:orange;
+  color:black;
+
+}
+
+/*extra small*/
+@media only screen and (max-width: 600px) {
+  
+  .subtitle {
+    padding-top: 10vw;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    overflow: hidden;
+    min-height: 90vh;
+  }
+
+  .pokedesign {
+    height: 2px;
+    margin-top: 4vw;
+    margin-bottom: 1vw;
+  }
+  .pokes img {
+    padding-top:3vw;
+    width: 20vw;
+    height: 20vw;
+    cursor: pointer;
+    padding: 3vw;
+
+    border: 0.2vw solid black;
+  }
+
+  footer {
+    min-height: 5vh;
+    background-color: black;
+    color: white;
+    font-size: 1.5vw;
+    font-family: montserrat;
+  }
+
+  #myRandom{
+   min-height: 90vh;
+}
+
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+
+   .subtitle {
+    padding-top: 10vw;
+  }
+
+  
+ .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    overflow: hidden;
+    min-height: 90vh;
+  }
+
+  .pokedesign {
+    height: 2px;
+    margin-top: 3vw;
+    margin-bottom: 1vw;
+  }
+  .pokes img {
+    padding-top:3vw;
+    width: 12vw;
+    height: 12vw;
+    cursor: pointer;
+    padding: 3vw;
+
+    border: 0.2vw solid black;
+  }
+
+  footer {
+    min-height: 5vh;
+   
+}
+}
+
+/* Medium devices (landscape tablets, 768px and up) 
+@media only screen and (min-width: 768px) {...}
+ */
 </style>
